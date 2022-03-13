b2477991acf141041f5f35da124645e2a57efdc0
Rahul Singh Bisht <93148343+hikigaya113@users.noreply.github.com>
2022-03-13 18:05:53 +0530

    Time: 14 ms (76.43%), Space: 10.9 MB (28.57%) - LeetHub

--- 1-two-sum/1-two-sum.cpp
+++ 1-two-sum/1-two-sum.cpp
@@ -4,16 +4,16 @@ public:
        int n=nums.size();
     vector<int>v;
         
+        unordered_map<int,int>m;
         for(int i=0;i<n;i++){
-      int k=target-nums[i];
-        for(int j=i+1;j<n;j++){
-            if(nums[j]==k){
+            if(m.find(target-nums[i])!=m.end()){
                 v.push_back(i);
-                v.push_back(j);
-                break;
+                v.push_back(m[target-nums[i]]);
+                return v;
             }
+            m[nums[i]]=i;
         }
-        }
+        
         return v;
     
     }
