6096974a3038f956bd0a807ec93ca12aff05b764
Rahul Singh Bisht <93148343+hikigaya113@users.noreply.github.com>
2022-04-29 15:34:57 +0530

    Added solution - LeetHub

--- Determine if Two Trees are Identical - GFG/determine-if-two-trees-are-identical.cpp
+++ Determine if Two Trees are Identical - GFG/determine-if-two-trees-are-identical.cpp
@@ -43,12 +43,13 @@ class Solution
     {
         //Your Code here
         
-        if(r1==NULL||r2==NULL) return (r1==r2);
+        if(r1==NULL and r2==NULL) return true;
+        
+        if(r1==NULL || r2==NULL ) return false;
         
         if(r1->data!=r2->data) return false;
         
-        return(isIdentical(r1->left,r2->left),isIdentical(r1->right,r2->right));
-        
+        return isIdentical(r1->left,r2->left) && isIdentical(r1->right,r2->right);
         
     }
 };
