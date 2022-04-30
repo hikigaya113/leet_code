f5c1701d7060f10cee84421e9f2049ca97de51da
Rahul Singh Bisht <93148343+hikigaya113@users.noreply.github.com>
2022-04-30 11:55:03 +0530

    Added solution - LeetHub

--- Symmetric Tree - GFG/symmetric-tree.cpp
+++ Symmetric Tree - GFG/symmetric-tree.cpp
@@ -105,40 +105,36 @@ class Solution{
     bool isSymmetric(struct Node* root)
     {
 	    // Code here
-	    
 	    Node*left,*right;
 	    if(!root) return true;
 	    
 	    queue<Node*>q1,q2;
+	    q1.push(root->left);
+	    q2.push(root->right);
 	    
-	        q1.push(root->left);
-	        q2.push(root->right);
+	    while(!q1.empty() && !q2.empty()){
 	        
-	        while(!q1.empty()&&!q2.empty()){
-	            
-	            left=q1.front();
-	            q1.pop();
-	            
-	            right=q2.front();
-	            q2.pop();
-	            
-	            if(NULL==left&&NULL==right)
-	            continue;
-	            
-	            if(NULL==left||NULL==right)
-	            return false;
-	            
-	            if(left->data!=right->data) return false;
-	            
-	            q1.push(left->left);
-	            q1.push(left->right);
-	            
-	            q2.push(right->right);
-	            q2.push(right->left);
-	            
-	        }
+	        left=q1.front();
+	        q1.pop();
+	        
+	        right=q2.front();
+	        q2.pop();
+	        
+	        if(NULL==left&&NULL==right) continue;
+	        
+	        if(NULL==left||NULL==right) return false;
+	        
+	        if(left->data!=right->data) return false;
+	        
+	        q1.push(left->left);
+	        q1.push(left->right);
+	        
+	        q2.push(right->right);
+	        q2.push(right->left);
+	    }
 	    
 	    return true;
+	    
     }
 };
 
