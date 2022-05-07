752baf55b21470e73667638a1098df911a49a8c9
Rahul Singh Bisht <93148343+hikigaya113@users.noreply.github.com>
2022-05-07 10:24:13 +0530

    Time: 131 ms (23.27%), Space: 38.8 MB (43.77%) - LeetHub

--- 456-132-pattern/456-132-pattern.cpp
+++ 456-132-pattern/456-132-pattern.cpp
@@ -3,31 +3,36 @@ public:
     bool find132pattern(vector<int>& nums) {
         
         
-        if(nums.empty()) return false;
+      int n=nums.size();
+//         int leftMin=nums[0];
+//         for(int j=1;j<n-1;j++){
+//             for(int k=j+1;k<n;k++){
+                
+//                 if(nums[k]>leftMin && nums[j]>nums[k])
+//                     return true;
+//             }
+//             leftMin=min(nums[j],leftMin);
+            
+//         }
+//         return false;
         
-        int n=nums.size();
         vector<int>ans(n);
-        
-        ans[0]=nums[0];
         stack<int>st;
         
+        ans[0]=nums[0];
         for(int i=1;i<n;i++){
             
             ans[i]=min(ans[i-1],nums[i]);
         }
-        for(int j=n-1;j>0;j--){
-            
-            while(!st.empty()&& nums[st.top()]<nums[j]){
-            if(nums[st.top()]>ans[j-1])
-                
-             return true;
-            st.pop();
+        
+            for(int j=n-1;j>=0;j--){
+                if(nums[j]>ans[j]){
+                    while(!st.empty()&&st.top()<=ans[j])
+                        st.pop();
+                    if(!st.empty()&&st.top()<nums[j]) return true;
+                    st.push(nums[j]);
+                }
             }
-            st.push(j);
-        }
         return false;
-        
-        
-        
     }
 };
