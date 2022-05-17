0ad9c5e6eb8833f87168eb6f9cc86aea99bfd6e3
Rahul Singh Bisht <93148343+hikigaya113@users.noreply.github.com>
2022-05-17 16:50:46 +0530

    Time: 20 ms (41.24%), Space: 17.6 MB (92.59%) - LeetHub

--- 42-trapping-rain-water/42-trapping-rain-water.cpp
+++ 42-trapping-rain-water/42-trapping-rain-water.cpp
@@ -1,43 +1,35 @@
 class Solution {
 public:
     int trap(vector<int>& height) {
+       int n=height.size();
+        int ans=0;
         
-//         int n=height.size();
-//         int waterTrapped=0;
-//         for(int i=0;i<n;i++){
-//           int j=i;
-//             int lefMax=0,rigMax=0;
-//             while(j>=0){
-//                 lefMax=max(lefMax,height[j]);
-//                           j--;
-//             }
-//             j=i;
-//             while(j<n){
-//                 rigMax=max(rigMax,height[j]);
-//                 j++;
-//             }
+        if(height.size()==0) return 0;
+        
+        int left=0,right=n-1;
+        
+        int leftMax=0,rightMax=0;
+        while(left<right){
             
-//             waterTrapped+=min(lefMax,rigMax)-height[i];
-//         }
-//         return waterTrapped;
-        
-        int n =height.size();
-        int prefix[n],suffix[n];
-        
-        prefix[0]=height[0];
-        for(int i=1;i<n;i++){
-            prefix[i]=max(prefix[i-1],height[i]);
-        }
-        
-        suffix[n-1]=height[n-1];
-        for(int i=n-2;i>=0;i--){
-            suffix[i]=max(suffix[i+1],height[i]);
+            if(height[left]>leftMax) leftMax=height[left];
             
+            if(height[right]>rightMax) rightMax=height[right];
+            
+            if(leftMax<rightMax){
+                ans+=max(0,leftMax-height[left]);
+                left++;
+            }
+            
+            else{
+                ans+=max(0,rightMax-height[right]);
+            right--;
+            }
+        
+        
         }
-        int waterTrapped=0;
-        for(int i=0;i<n;i++){
-            waterTrapped+=min(prefix[i],suffix[i])-height[i];
-}
-        return waterTrapped;
+         
+        return ans;
+          
     }
-};
+    
+    };
