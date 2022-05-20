7e4b4d9c993a9b018d89421b330f8c237c83f641
Rahul Singh Bisht <93148343+hikigaya113@users.noreply.github.com>
2022-05-20 16:15:47 +0530

    Time: 4 ms (77.87%), Space: 9.7 MB (73.12%) - LeetHub

--- 35-search-insert-position/35-search-insert-position.cpp
+++ 35-search-insert-position/35-search-insert-position.cpp
@@ -4,19 +4,22 @@ public:
         
         int n=nums.size();
         
-        int low=0,high=n-1,mid,insert=0;
+        int low=0,high=n-1,mid;
         
         while(low<=high){
             mid=(low+high)/2;
             
             if(nums[mid]==target) return mid;
             
-            if(nums[mid]>target)
+            if(nums[mid]>target){
                 high=mid-1;
+            
+        }
             else {low=mid+1;
-                 insert=mid+1;
+                 ;
                  }
         }
-        return insert;
+        
+        return low;
     }
 };
