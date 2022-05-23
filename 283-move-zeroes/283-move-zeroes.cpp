6a49d15027cff35dfa9342721f5709bd6a1d2f01
Rahul Singh Bisht <93148343+hikigaya113@users.noreply.github.com>
2022-05-23 15:46:30 +0530

    Time: 35 ms (41.81%), Space: 20.5 MB (5.46%) - LeetHub

---
+++ 283-move-zeroes/283-move-zeroes.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        
+        int n=nums.size();
+        
+        int count=0;
+        for(int i=0;i<n;i++){
+            if(nums[i]==0) count++;
+        }
+        
+        vector<int>ans;
+        for(int i=0;i<n;i++){
+            if(nums[i]!=0) ans.push_back(nums[i]);
+        }
+        while(count--){
+            ans.push_back(0);
+        }
+        
+        for(int i=0;i<n;i++){
+            nums[i]=ans[i];
+        }
+    }
+};
