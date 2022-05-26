a1977efaa480332debfac9c8231e4dff846f6a26
Rahul Singh Bisht <93148343+hikigaya113@users.noreply.github.com>
2022-05-26 17:12:10 +0530

    Time: 8 ms (75.56%), Space: 14.8 MB (44.29%) - LeetHub

--- 21-merge-two-sorted-lists/21-merge-two-sorted-lists.cpp
+++ 21-merge-two-sorted-lists/21-merge-two-sorted-lists.cpp
@@ -12,20 +12,41 @@ class Solution {
 public:
     ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
         
+//         if(list1==NULL) return list2;
+//         if(list2==NULL) return list1;
+//         if(list1->val>list2->val ) swap(list1,list2);
+        
+        
+//               ListNode* ans=list1;;
+//               while(list1!=NULL&&list2!=NULL){
+//                   ListNode*tmp=NULL;
+//                   while(list1!=NULL&&list1->val<=list2->val){
+//                       tmp=list1;
+//                       list1=list1->next;
+//                   }
+//                   tmp->next=list2;
+//                   swap(list1,list2);
+                  
+//               }       
+//               return ans;
+        
+        
         if(list1==NULL) return list2;
         if(list2==NULL) return list1;
-        if(list1->val>list2->val ) swap(list1,list2);
-              ListNode* ans=list1;;
-              while(list1!=NULL&&list2!=NULL){
-                  ListNode*tmp=NULL;
-                  while(list1!=NULL&&list1->val<=list2->val){
-                      tmp=list1;
-                      list1=list1->next;
-                  }
-                  tmp->next=list2;
-                  swap(list1,list2);
-                  
-              }       
-              return ans;
+        
+        if(list1->val>list2->val) swap(list1,list2);
+        
+        ListNode* ans=list1;
+        while(list1!=NULL &&list2!=NULL){
+              ListNode*temp=NULL;
+            while(list1!=NULL&&list1->val<=list2->val){
+                temp=list1;
+                list1=list1->next;
+            }
+            temp->next=list2;
+            swap(list1,list2);
+            
+            }
+        return ans;
     }
 };
