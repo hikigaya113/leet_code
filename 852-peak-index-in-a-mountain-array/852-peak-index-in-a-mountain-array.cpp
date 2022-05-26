914df09c4f6aa729f7366f393ae2dac6a1fd1ce7
Rahul Singh Bisht <93148343+hikigaya113@users.noreply.github.com>
2022-05-26 10:41:36 +0530

    Time: 15 ms (44.31%), Space: 11.6 MB (49.63%) - LeetHub

---
+++ 852-peak-index-in-a-mountain-array/852-peak-index-in-a-mountain-array.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int peakIndexInMountainArray(vector<int>& arr) {
+     
+        int n=arr.size();
+        if(n<3) return -1;
+        
+        int i=0;
+        while(arr[i]<arr[i+1]) i++;
+        return i;
+    }
+};
