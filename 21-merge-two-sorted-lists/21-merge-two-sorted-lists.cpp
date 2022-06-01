6ffefd494e65c9c9e7adc34af2b15df2513db730
Rahul Singh Bisht <93148343+hikigaya113@users.noreply.github.com>
2022-06-01 10:32:31 +0530

    Time: 13 ms (36.49%), Space: 14.9 MB (44.13%) - LeetHub

--- 21-merge-two-sorted-lists/21-merge-two-sorted-lists.cpp
+++ 21-merge-two-sorted-lists/21-merge-two-sorted-lists.cpp
@@ -10,43 +10,32 @@
  */
 class Solution {
 public:
-    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+    ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
+        
+         if(l1 == NULL) return l2;
+        if(l2 == NULL) return l1;
+        if(l1->val>=l2->val) l2->next = mergeTwoLists(l1, l2-> next);
+        else{
+            l1->next = mergeTwoLists(l1->next, l2);
+            l2 = l1;
+        }return l2;
+        
         
 //         if(list1==NULL) return list2;
 //         if(list2==NULL) return list1;
-//         if(list1->val>list2->val ) swap(list1,list2);
         
         
-//               ListNode* ans=list1;;
-//               while(list1!=NULL&&list2!=NULL){
-//                   ListNode*tmp=NULL;
-//                   while(list1!=NULL&&list1->val<=list2->val){
-//                       tmp=list1;
-//                       list1=list1->next;
-//                   }
-//                   tmp->next=list2;
-//                   swap(list1,list2);
-                  
-//               }       
-//               return ans;
-        
-        
-        if(list1==NULL) return list2;
-        if(list2==NULL) return list1;
-        
-        if(list1->val>list2->val) swap(list1,list2);
-        
-        ListNode* ans=list1;
-        while(list1!=NULL &&list2!=NULL){
-              ListNode*temp=NULL;
-            while(list1!=NULL&&list1->val<=list2->val){
-                temp=list1;
-                list1=list1->next;
-            }
-            temp->next=list2;
-            swap(list1,list2);
+//         ListNode* ans=list1;
+//         while(list1!=NULL &&list2!=NULL){
+//               ListNode*temp=NULL;
+//             while(list1!=NULL&&list1->val<=list2->val){
+//                 temp=list1;
+//                 list1=list1->next;
+//             }
+//             temp->next=list2;
+//             swap(list1,list2);
             
-            }
-        return ans;
+//             }
+//         return ans;
     }
 };
