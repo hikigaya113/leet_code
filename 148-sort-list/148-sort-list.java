9cd4b8e544579a9313c60272ea5f4169ea33fd88
Rahul Singh Bisht <93148343+hikigaya113@users.noreply.github.com>
2022-06-01 12:18:49 +0530

    Time: 99 ms (5.04%), Space: 74.6 MB (27.62%) - LeetHub

---
+++ 148-sort-list/148-sort-list.java
@@ -0,0 +1,30 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode sortList(ListNode head) {
+          ListNode curr = head;
+        Queue<ListNode> queue = new PriorityQueue<>((n1, n2) -> n1.val - n2.val); 
+        while (curr != null) {
+            queue.add(curr);
+            curr = curr.next;
+        }
+        
+        ListNode dummy = new ListNode();
+        ListNode prev = dummy;
+        while (!queue.isEmpty()) {
+            curr = queue.poll();
+            curr.next = null;
+            prev.next = curr;
+            prev = curr;
+        }
+        return dummy.next;
+    }
+}
