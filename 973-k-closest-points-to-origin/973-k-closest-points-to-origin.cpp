2e2c9080516d8156a61158a7dea28424c8e78d67
Rahul Singh Bisht <93148343+hikigaya113@users.noreply.github.com>
2022-06-06 05:41:23 +0530

    Time: 260 ms (73.63%), Space: 57 MB (92.78%) - LeetHub

---
+++ 973-k-closest-points-to-origin/973-k-closest-points-to-origin.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<vector<int>> kClosest(vector<vector<int>>& points, int k) {
+        
+         sort(points.begin(), points.end(), [&](vector<int>& a, vector<int>& b) {
+            return squaredDistance(a) < squaredDistance(b);
+        });
+        
+        // Return the first k elements of the sorted vector
+        return vector<vector<int>>(points.begin(), points.begin() + k);
+    }
+    
+private:
+    int squaredDistance(vector<int>& point) {
+        // Calculate and return the squared Euclidean distance
+        return point[0] * point[0] + point[1] * point[1];  
+        
+    }
+};
