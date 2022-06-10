00fdc8b57f99386f2987989c7bec0a518888f02c
Rahul Singh Bisht <93148343+hikigaya113@users.noreply.github.com>
2022-06-10 06:11:55 +0530

    Time: 23 ms (57.32%), Space: 8.3 MB (59.36%) - LeetHub

--- 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.cpp
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.cpp
@@ -1,50 +1,28 @@
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
-//         int n = s.length();
+        int n = s.length();
         
-//         int ans=0,start=0;
-//         unordered_map<char,int>m;
-        
-//         if(n<=1)
-//           return n;
-        
-//         for(int i=0;i<n;i++){
-//         if(m.count(s[i]))
-//         {
-//             start=max(start,m[s[i]]+1);
-//             m[s[i]]==i;
-//         }
-            
-//             else{
-//                 m[s[i]]==i;
-//             }
-//             ans=max(ans,i-start+1);
-//         }
-//         return ans;
-        
-         int n=s.size();
         int ans=0,start=0;
-        unordered_map<char,int> m; // key = string character, value = character index
+        unordered_map<char,int>m;
         
         if(n<=1)
           return n;
         
-        for(int i=0; i<n; i++)
+        for(int i=0;i<n;i++){
+        if(m.count(s[i]))
         {
-            if(m.count(s[i]))   //If there's a repeated character
-            {
-                //Substring start index is prev index of repeated character + 1
-                start=max(start,m[s[i]]+1);  
-                m[s[i]]=i;    //Updating the index of the repeated character
-            }
-            else
-            {
+            start=max(start,m[s[i]]+1);
+            m[s[i]]=i;
+        }
+            
+            else{
                 m[s[i]]=i;
             }
-                
-            ans=max(ans,i-start+1);    //Since end index of the current substring is i 
+            ans=max(ans,i-start+1);
         }
         return ans;
+        
+        
     }
 };
