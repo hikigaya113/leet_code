262103368b943984ff369efcb73e9b071c107c5a
Rahul Singh Bisht <93148343+hikigaya113@users.noreply.github.com>
2022-06-11 11:18:35 +0530

    Time: 4 ms, Memory: 8 MB - LeetHub

---
+++ fizz-buzz/fizz-buzz.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<string> fizzBuzz(int turnedOn) {
+     vector<string>ans;
+        for(int i=1;i<=turnedOn;i++){
+            
+             if(i%3==0&&i%5==0) ans.push_back("FizzBuzz");
+            
+             else if(i%3==0)
+                ans.push_back("Fizz");
+            
+            else if(i%5==0)
+                ans.push_back("Buzz");
+            
+            else
+                ans.push_back(to_string (i));
+        }
+        return ans;
+    }
+};
