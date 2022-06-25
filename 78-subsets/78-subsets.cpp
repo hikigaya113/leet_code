12605d73ffd492bf3d8cf916a6c81a5fc4ca2f8f
Rahul Singh Bisht <93148343+hikigaya113@users.noreply.github.com>
2022-06-25 18:02:41 +0530

    Time: 4 ms (53.10%), Space: 6.9 MB (99.38%) - LeetHub

--- 78-subsets/78-subsets.cpp
+++ 78-subsets/78-subsets.cpp
@@ -2,28 +2,48 @@ class Solution {
 public:
     vector<vector<int>> subsets(vector<int>& nums) {
         
+        
+        // BACKTRACKING
+        
+//         int n=nums.size();
+//         vector<vector<int>>ans;
+//         vector<int>v;
+        
+// //         subset(nums,ans,v,0);
+// //         return ans;
+            
+// //         }
+    
+// //     void subset(vector<int>&nums,vector<vector<int>>&ans,vector<int>v,int start){
+        
+// //         if(start==nums.size()){
+// //             ans.push_back(v);
+// //             return;
+// //         }
+        
+// //         v.push_back(nums[start]);
+// //         subset(nums,ans,v,start+1);
+        
+// //         v.pop_back();
+// //         subset(nums,ans,v,start+1);
+        
+        
+        //ITERATIVE
+        
         int n=nums.size();
         vector<vector<int>>ans;
         vector<int>v;
-        
-        subset(nums,ans,v,0);
+        ans.push_back({});
+        for(int i=0;i<n;i++){
+
+            int s=ans.size();
+            for(int j=0;j<s;j++){
+                v=ans[j];
+                v.push_back(nums[i]);
+                ans.push_back(v);
+            }
+        }
         return ans;
-            
-        }
-    
-    void subset(vector<int>&nums,vector<vector<int>>&ans,vector<int>v,int start){
-        
-        if(start==nums.size()){
-            ans.push_back(v);
-            return;
-        }
-        
-        v.push_back(nums[start]);
-        subset(nums,ans,v,start+1);
-        
-        v.pop_back();
-        subset(nums,ans,v,start+1);
-        
         
 }
     
