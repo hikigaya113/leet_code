5f24a3f131f264fb6578078ff863a0a7173b83cf
Rahul Singh Bisht <93148343+hikigaya113@users.noreply.github.com>
2022-07-11 13:16:06 +0530

    Added solution - LeetHub

---
+++ Karatsuba Algorithm - GFG/karatsuba-algorithm.java
@@ -0,0 +1,37 @@
+// { Driver Code Starts
+//Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+import java.lang.*;
+
+class GFG {
+    public static void main(String args[]) throws IOException {
+        BufferedReader read =
+            new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        while (t-- > 0) {
+            String S[] = read.readLine().split(" ");
+            String A = S[0];
+            String B = S[1];
+
+            Solution ob = new Solution();
+            System.out.println(ob.karatsubaAlgo(A,B));
+        }
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution {
+    static Long karatsubaAlgo(String A, String B) {
+        // code here
+        
+         long  a = Long.parseLong(A,2);
+        long  b = Long.parseLong(B,2);
+        long ans = a*b;
+        return ans;
+    }
+};
