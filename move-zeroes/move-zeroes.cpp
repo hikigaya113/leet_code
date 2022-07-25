0408781498e077958711779742e5b74502424d6f
Rahul Singh Bisht <93148343+hikigaya113@users.noreply.github.com>
2022-07-25 09:58:01 +0530

    Time: 31 ms, Memory: 20.6 MB - LeetHub

--- move-zeroes/move-zeroes.cpp
+++ move-zeroes/move-zeroes.cpp
@@ -2,52 +2,21 @@ class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
         
-        // sc=O(n),tc=O(n);
-//         int n=nums.size();
-        
-//         int count=0;
-//         for(int i=0;i<n;i++){
-//             if(nums[i]==0) count++;
-//         }
-        
-//         vector<int>ans;
-//         for(int i=0;i<n;i++){
-//             if(nums[i]!=0) ans.push_back(nums[i]);
-//         }
-//         while(count--){
-//             ans.push_back(0);
-//         }
-        
-//         for(int i=0;i<n;i++){
-//             nums[i]=ans[i];
-//         }
-        
-        // optimal solution
-        
-//         int lastnonezero=0;
-        
-//         for(int i=0;i<nums.size();i++){
-//             if(nums[i]!=0) { nums[lastnonezero]=nums[i];
-//                             lastnonezero++;
-                            
-//                             }
-//         }
-        
-//         for(int i=lastnonezero;i<nums.size();i++){
-//             nums[i]=0;
-//         }
-        
-        
-        int n=0;
-        for(int i=0;i<nums.size();i++){
-            if(nums[i]!=0) {
-               nums[n]=nums[i];
-                n++;
+        int n=nums.size();
+        int count=0;
+        vector<int>ans;
+        for(int i=0;i<n;i++){
+            if(nums[i]==0){
+                count++;
+                continue;
             }
+            else
+                ans.push_back(nums[i]);
+               
         }
-        
-        for(int i=n;i<nums.size();i++){
-            nums[i]=0;
+        for(int i=0;i<count;i++){
+            ans.push_back(0);
         }
+        nums=ans;
     }
 };
