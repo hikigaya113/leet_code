d528c86a4ad5d17849ce78c176067f333e72f835
Rahul Singh Bisht <93148343+hikigaya113@users.noreply.github.com>
2022-07-26 13:11:49 +0530

    Time: 19 ms, Memory: 7.3 MB - LeetHub

--- valid-anagram/valid-anagram.cpp
+++ valid-anagram/valid-anagram.cpp
@@ -1,27 +1,19 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-//         unordered_map<char,int>bisht;
         
+        int n=s.length();
+        int m=t.length();
         
-//         int n=s.length();
-//         if(t.length()!=n) return false;
-        
-//         for(int i=0;i<n;i++){
-//            bisht[s[i]]++;
-//             bisht[t[i]]--;
-//         }
-        
-//         for(auto val:bisht){
-//             if(val.second) return false;
-            
-//         }
-//         return true;
+        if(n<m)return false; 
         
         sort(s.begin(),s.end());
         sort(t.begin(),t.end());
         
-        return s==t;
-            
+        for(int i=0;i<n;i++){
+            if(s[i]!=t[i])
+                return false;
+        }
+        return true;
     }
 };
