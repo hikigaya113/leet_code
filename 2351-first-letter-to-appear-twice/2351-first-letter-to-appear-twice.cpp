c50e0c5ccd5338269b68a57d364617f258739c3d
Rahul Singh Bisht <93148343+hikigaya113@users.noreply.github.com>
2022-07-26 10:00:04 +0530

    Time: 5 ms (25.00%), Space: 6 MB (75.00%) - LeetHub

---
+++ 2351-first-letter-to-appear-twice/2351-first-letter-to-appear-twice.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    char repeatedCharacter(string s) {
+        
+        int n=s.length();
+        map<char,int>mp;
+        for(int i=0;i<n;i++){
+            mp[s[i]]++;
+           
+            if(mp[s[i]]==2)
+                return s[i];
+        
+        
+        }
+        
+        
+        return '-1';
+    }
+};
